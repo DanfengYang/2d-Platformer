12a2143c5d5f497026bdfd796443620cd7b8c733
DanfengYang <dy1123@nyu.edu>
2022-02-18 00:19:34 -0500

    Final Tuning of numbers and level design

--- Assets/Scripts/WinningTrigger.cs
+++ Assets/Scripts/WinningTrigger.cs
@@ -10,15 +10,20 @@ public class WinningTrigger: MonoBehaviour
     [SerializeField] private TMP_Text winningText;
     [SerializeField] private TMP_FontAsset font1;
 
-    public float timeRemaining = 5;
+    public float timeRemaining = 10;
     string youWonText = "You Won!!!";
 
     bool won = false;
+
+    GameObject player;
+    GainItem gainitem;
 
 
     void Start()
     {
         winningText.font = font1;
+        player = GameObject.FindGameObjectWithTag("Player");
+        gainitem = player.GetComponent<GainItem>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -38,6 +43,7 @@ public class WinningTrigger: MonoBehaviour
         {
             winningText.text = youWonText;
             timeRemaining -= Time.deltaTime;
+            gainitem.itemNumber = 0;
         }
 
         if (timeRemaining <= 0)
